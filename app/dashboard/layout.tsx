7ca53445f755c4816c0f91552d5cae4400d8aa8a
devs0ft <devsoft78@gmail.com>
2024-07-26 05:02:10 -0800

    new

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,17 @@
 import Header from '@/components/Header'
+import { ClerkLoaded } from '@clerk/nextjs'
 import React, { ReactNode } from 'react'
 
 function DashboardLayout({children}:{children:React.ReactNode}) {
   return (
-    <div className='flex-1 flex flex-col h-screen'>
+    <ClerkLoaded>
+ <div className='flex-1 flex flex-col h-screen'>
       <Header/>
-      <main>
+      <main className='flex-1 overflow-y-auto'>
         {children}
     </main>
     </div>
+    </ClerkLoaded>
   )
 }
 
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
+export const dynamic = "force-dynamic"
 import Documents from '@/components/Documents'
 import React from 'react'
 
 function Dashboard() {
   return (
-    <div className='mx-auto max-w-7xl p-5 bg-gray-200 font-extralight text-2xl'>
+    <div className='mx-auto h-full max-w-7xl p-5 bg-gray-200 font-extralight text-2xl'>
      <div>
       <h2 className='text-indigo-600'>My Documents</h2>
       <Documents/>
